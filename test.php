81c0261760611e49b56c75c41727c212924ceb83
lraby <lraby11@gmail.com>
2019-07-17 15:25:21 -0500

    Added mailgun lib

--- test.php
+++ test.php
@@ -9,10 +9,9 @@ $domain = "sandboxf866b496097e4d1d969c61a8d965cd4a.mailgun.org";
 
 # Make the call to the client.
 $result = $mgClient->sendMessage("$domain",
-          array('from'    => 'Mailgun Sandbox <postmaster@sandboxdf866b496097e4d1d969c61a8d965cd4a.mailgun.org>',
+          array('from'    => 'Mailgun Sandbox <postmaster@sandboxf866b496097e4d1d969c61a8d965cd4a.mailgun.org>',
                 'to'      => 'Leesa <lraby11@gmail.com>',
                 'subject' => 'Hello Leesa',
                 'text'    => 'Congratulations Leesa, you just sent an email with Mailgun! You are truly awesome!'));
 
 var_dump($result);
-
