6424a3590c37846236e7dde914f52b534adaacd1
lraby <lraby11@gmail.com>
2019-07-17 16:16:55 -0500

    Added Read Me

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Leesa-Raby.github.io
 My GitHub Pages Site
+# example.com
+
+An about me web site.
+
+## Dependencies
+
+* PHP 7.0 or higher
+* Composer
+* An API key for Mailgun
+
+## Installation
+
+Clone this project into a web directory
+```sh
+cd /var/www
+git clone git@github.com:lraby/example.com.git
--- test.php
+++ test.php
@@ -8,10 +8,16 @@ $mgClient = new Mailgun('cff823e776d576fd37028895b45d8d68-fd0269a6-a8b73a6c'); /
 $domain = "sandboxf866b496097e4d1d969c61a8d965cd4a.mailgun.org";
 
 # Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-          array('from'    => 'Mailgun Sandbox <postmaster@sandboxf866b496097e4d1d969c61a8d965cd4a.mailgun.org>',
-                'to'      => 'Leesa <lraby11@gmail.com>',
-                'subject' => 'Hello Leesa',
-                'text'    => 'Congratulations Leesa, you just sent an email with Mailgun! You are truly awesome!'));
+$from = "Mailgun Sandbox <postmaster@{$domain}>";
+$to = 'Leesa <lraby11@gmail.com>';
+$subject = 'Hello Leesa';
+$text = 'Congratulations Leesa, you just sent an email with Mailgun! You are truly awesome!';
 
-var_dump($result);
+$result = $mgClient->sendMessage(
+  $domain,
+  array('from'    => $from,
+        'to'      => $to,
+        'subject' => $subject,
+        'text'    => $text
+    )
+);
